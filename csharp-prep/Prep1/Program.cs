c70196794e10bf750f283a03894c7fe868687695
trebmale37 <trebmale37@gmail.com>
2024-09-18 17:48:14 +0000

    Some changes made to the display

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Decalring variables
+        string firstname;
+        string lastname;
+
+        //Prompting the user for their first name
+        Console.Write($"What is your your first name? ");
+        firstname = Console.ReadLine();
+
+        //Prompting the user fir their last name
+        Console.Write($"What is your last name?  ");
+        lastname = Console.ReadLine();
+
+        Console.WriteLine();
+        //Printing the user last name and then thier first name in a sentence
+        Console.WriteLine($"Your name is {firstname}, {firstname} {lastname}");
+
+        //Prompting the user for their first Name
+        Console.Write($"What is your first name? ");
+        firstname = Console.ReadLine();
+
+        //Prompting the user for their last name
+        Console.Write($"What is your last name? ");
+        lastname = Console.ReadLine();
+
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}");
     }
 }
