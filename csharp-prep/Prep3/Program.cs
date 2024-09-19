3b5a8e9a568dad12af5eed990eb048fad956b032
trebmale37 <trebmale37@gmail.com>
2024-09-19 11:08:37 +0000

    Ramdom guess games completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        /*Start by asking the user for the magic number. 
+        Ask the user for a guess.
+        Using an if statement, determine if the user needs to guess higher or lower 
+        next time, or tell them if they guessed it.
+        */
+
+        //initializing the random function
+        Random magic = new Random();
+        int magicnumber = magic.Next(1,101);
+
+        //declaring a variable called magic number
+        string number;
+        int guess;
+        do{
+            //Asking the user for a guess
+            //converting the string to integer
+            Console.Write($"What is your guess? ");
+            number = Console.ReadLine();
+            guess = int.Parse(number);
+
+            //Using a do while loop
+            
+                //determine if the user needs to guess higher or lower
+                //or tell them if they guessed it
+                if(guess == magicnumber){
+                    Console.WriteLine($"You guessed it");
+                }
+                else if(guess < magicnumber){
+                    Console.WriteLine($"Higher");
+                }
+                else{
+                    Console.WriteLine($"Lower");
+                }
+            }while(guess != magicnumber);
     }
 }
